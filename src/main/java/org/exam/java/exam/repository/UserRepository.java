8d984e8012c8fa56a1ef0ad36a98fa3d80e8acd5
luigicaiaffa <luigicaiaffa8@gmail.com>
2025-05-05 10:44:21 +0200

    Add userDetailService

--- src/main/java/org/exam/java/exam/repository/UserRepository.java
+++ src/main/java/org/exam/java/exam/repository/UserRepository.java
@@ -1,8 +1,11 @@
 package org.exam.java.exam.repository;
 
+import java.util.Optional;
+
 import org.exam.java.exam.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Integer> {
     
+    public Optional<User> findByUsername(String username);
 }
---
+++ src/main/java/org/exam/java/exam/security/DatabaseUserDetailService.java
@@ -0,0 +1,29 @@
+package org.exam.java.exam.security;
+
+import java.util.Optional;
+
+import org.exam.java.exam.model.User;
+import org.exam.java.exam.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class DatabaseUserDetailService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
+        Optional<User> userAttempt = userRepository.findByUsername(username);
+
+        if (userAttempt.isEmpty()) {
+            throw new UsernameNotFoundException("Unimplemented method");
+        }
+
+        return "";
+    }
+}
