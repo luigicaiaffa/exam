22d19989d01dcee6eee82d93a4367d33895f3830
luigicaiaffa <luigicaiaffa8@gmail.com>
2025-05-07 17:48:33 +0200

    Add homepage layout

--- src/main/java/org/exam/java/exam/controller/MainController.java
+++ src/main/java/org/exam/java/exam/controller/MainController.java
@@ -1,5 +1,15 @@
 package org.exam.java.exam.controller;
 
+import java.math.BigDecimal;
+import java.util.List;
+import java.util.Map;
+
+import org.exam.java.exam.model.Exam;
+import org.exam.java.exam.model.User;
+import org.exam.java.exam.service.ExamService;
+import org.exam.java.exam.service.GradeService;
+import org.exam.java.exam.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -10,9 +20,32 @@ import org.springframework.web.bind.annotation.GetMapping;
 @RequestMapping
 public class MainController {
 
+    @Autowired
+    private UserService userService;
+
+    @Autowired
+    private ExamService examService;
+
+    @Autowired
+    private GradeService gradeService;
+
     @GetMapping("/")
     public String homePage(Authentication auth, Model model) {
-        model.addAttribute("user", auth.getName());
+
+        User user = userService.findByUsername(auth.getName()).get();
+        Integer userId = user.getId();
+
+        List<Exam> examsToDo = examService.findUserExamsToDo(userId);
+        List<Exam> examsPassed = examService.findUserExamsWithGrade(userId);
+
+        Map<String, BigDecimal> averages = gradeService.getAveragesByUserId(userId);
+        model.addAttribute("weightedAvg", averages.get("weighted"));
+        model.addAttribute("totalCfu", averages.get("totalCfu"));
+
+        model.addAttribute("user", user);
+        model.addAttribute("examsToDo", examsToDo);
+        model.addAttribute("examsPassed", examsPassed);
+
         return "/main/homepage";
     }
 
